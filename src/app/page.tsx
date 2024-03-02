c031080b07c8158d7ff4ab9002f08042bd0141f8
joao nishimoto <jvonishimoto@gmail.com>
2024-03-02 11:35:23 -0300

    divide in 3 columns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-import Image from "next/image";
+import { useState } from 'react';
+import Detail from "@/components/Detail";
+import SidebarChecklist from "@/components/SidebarChecklist";
+import SidebarParts from "@/components/SidebarParts";
+
 
 export default function Home() {
+
+  const [sidebarHidden, setSidebarHidden] = useState(false);
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1>hello world!</h1>
+    <div className="min-h-screen grid grid-cols-5 lg:grid-cols-7">
+      {!sidebarHidden && <SidebarParts />}
+      <Detail />
+      <SidebarChecklist />
+      <button onClick={() => setSidebarHidden(!sidebarHidden)}>Toggle Sidebar</button>
     </div>
   );
 }
